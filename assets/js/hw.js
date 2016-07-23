52f74c64bb09c1be1518f6afe94f02a07aadab39
CodingBootCamp <codingbootcamp@Jorges-MacBook-Pro.local>
2016-07-23 12:05:43 -0400

    added spacing between buttons; made it super hero themed

--- assets/js/hw.js
+++ assets/js/hw.js
@@ -3,23 +3,23 @@ $(document).ready(function()
 	// global variables
 	var apiKey = 'dc6zaTOxFJmzC';
 	var queryUrl = 'https://api.giphy.com/v1/gifs/search?q=%query%&api_key='+apiKey;
-	var animalsArray = ['dog', 'cat', 'rabbit', 'hamster', 'skunk', 'goldfish', 'bird', 'ferret', 'turtle', 'sugar glider', 'chinchilla', 'hedgehog', 'hermit crab', 'gerbil', 'pygmy goat', 'chicken', 'capybara', 'teacup pig', 'serval', 'salamander', 'frog'];
-	var numGifs = 12;
+	var superHeroesArray = ['batman', 'spiderman', 'superman', 'iron man', 'wolverine', 'captain america', 'hulk', 'flash', 'thor', 'deadpool'];
+	var numberOfGifs = 12;
 	var gifsArray = [];
 
-	// dynamically adds buttons and appends to the animalButtons div
-	for(var i = 0; i < animalsArray.length; i++)
+	// dynamically adds buttons and appends to the heroButtons div
+	for(var i = 0; i < superHeroesArray.length; i++)
 	{
-		$('<a>').addClass('btn btn-primary').text(animalsArray[i]).appendTo($('#animalButtons'));
+		addHeroButton(superHeroesArray[i]);
 	}
 
-	// assigns action to animal-form submit event
-	$('#animal-form').submit(function(event)
+	// assigns action to hero-form submit event
+	$('#hero-form').submit(function(event)
 	{
-		var animalInput = $('#animal-input');
+		var heroInput = $('#hero-input').val();
 
-		// adds a button with the text that was entered in the animalInput input element by accessing it's val attribute
-		$('#animalButtons').append($('<a>').addClass('btn btn-primary').text(animalInput.val()));
+		// adds a button with the text that was entered in the heroInput input element by accessing it's val attribute
+		addHeroButton(heroInput);
 
 		// javascript method to reset fields in a form
 		this.reset();
@@ -28,24 +28,36 @@ $(document).ready(function()
 		return false;
 	});
 
-	// assigns click even action for all 'a' elements in the animalButtons div
-	$('#animalButtons').on('click', 'a', function()
+	// function to be called whenever adding an hero button
+	function addHeroButton(heroText)
 	{
-		// emtpies the animals div of any elements inside
-		$('#animals').empty();
+		// creating the jquery object
+		var heroButton = $('<a>').addClass('btn btn-primary').text(heroText);
+		// appends the object to the hero buttons div, making it actually exist because...
+		$('#heroButtons').append(heroButton);
+
+		// ... this doesn't work unless the jquery object is already on the dom, which adds a little space between buttons without having to add a margin
+		heroButton.after(' ');
+	}
+
+	// assigns click even action for all 'a' elements in the heroButtons div
+	$('#heroButtons').on('click', 'a', function()
+	{
+		// emtpies the heroes div of any elements inside
+		$('#heroes').empty();
 
 		// gets the text of the specific 'a' element that was clicked
-		var animal = $(this).text();
+		var hero = $(this).text();
 
-		// ajax call to giphy; replaces %query% with the string stored in animal variable
-		$.ajax({url: queryUrl.replace('%query%', animal), method:'GET'})
+		// ajax call to giphy; replaces %query% with the string stored in hero variable
+		$.ajax({url: queryUrl.replace('%query%', hero), method:'GET'})
 		.done(function(response)
 		{
 			// returns a shuffled copy of the array contained in the response data property
 			var gifs = knuthShuffle(response.data);
 
 			// loops for the number of gifs that we want, relegating incremeting i to another for loop inside
-			for(var i = 0; i < numGifs;)
+			for(var i = 0; i < numberOfGifs;)
 			{
 				//creates a row for every 3 gifs for nicer display
 				var row = $('<div>').addClass('row');
@@ -54,12 +66,12 @@ $(document).ready(function()
 				for(var j = 0; j < 3; j++, i++)
 				{
 					// break out of the loop as we no longer need to add anymore gifs
-					if(i == numGifs)
+					if(i == numberOfGifs)
 						break;
 
-					// create column divs and append it to the row which is append to the animals div
+					// create column divs and append it to the row which is append to the heroes div
 					var column = $('<div>').addClass('col-md-3');
-					$('#animals').append(row.append(column));
+					$('#heroes').append(row.append(column));
 
 					// obtain info from gifs array that contains information from the response data attribute
 					// set rating to 'E' if none is found
@@ -84,8 +96,8 @@ $(document).ready(function()
 		});
 	});
 
-	// sets behavior for click events for any imgs clicked within the animals div
-	$('#animals').on('click', 'img', function()
+	// sets behavior for click events for any imgs clicked within the heroes div
+	$('#heroes').on('click', 'img', function()
 	{
 		// checks if the img src attribute is the still img and sets it to the gif url
 		if($(this).attr('src') === $(this).attr('data-still'))
